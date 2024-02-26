1499550a516f85e9bd4412bf2c5969c51aef11ad
Quinten <quinten.roets@gmail.com>
2024-02-25 19:57:46 -0800

    adapt to new superpathlib name

--- src/music/models/options.py
+++ src/music/models/options.py
@@ -10,13 +10,13 @@ clean_download_ids_help = (
     "This command saves the most recent id of all downloaded tracks"
 )
 
+clean_download_ids_option = typer.Option(help=clean_download_ids_help)
+
 
 @dataclass
 class Options:
     config_path: Path = Path.config
     upload_to_phone: bool = True
     add: Annotated[str | None, typer.Option(help="song to add")] = None
-    clean_download_ids: Annotated[
-        bool, typer.Option(help=clean_download_ids_help)
-    ] = False
+    clean_download_ids: Annotated[bool, clean_download_ids_option] = False
     fix_mtimes_on_phone: bool = False
