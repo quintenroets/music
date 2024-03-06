33ae48cb70a47b245a74371067ce05593de923c6
Quinten <quinten.roets@gmail.com>
2024-03-05 22:16:26 -0800

    adapt to new webapp-starter

--- src/music/cli/webapp.py
+++ src/music/cli/webapp.py
@@ -1,10 +1,10 @@
 from package_utils.context.entry_point import create_entry_point
 from webapp_starter.context.context import Context_, context
 from webapp_starter.main.main import main
-from webapp_starter.models import Config, Options
+from webapp_starter.models import Config, Options, Secrets
 
 
-def add_name(created_context: Context_[Options, Config, None]) -> None:
+def add_name(created_context: Context_[Options, Config, Secrets]) -> None:
     created_context.options.name = "music"
 
 
