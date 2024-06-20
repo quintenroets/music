4ba0686819784bdd0e9f324b22f1eabca6746306
Quinten Roets <62651391+quintenroets@users.noreply.github.com>
2024-06-20 00:26:49 -0700

    Run in ci (#25)
    use actions from main

--- src/music/cli/entry_point.py
+++ src/music/cli/entry_point.py
@@ -1,6 +1,17 @@
-from package_utils.context.entry_point import create_entry_point
+import os
+
+from package_utils.context.entry_point import Context, create_entry_point
 
 from music.context import context
 from music.main import main
+from music.models import Config, Options, Secrets
 
-entry_point = create_entry_point(main, context)
+
+def configure_no_secret_file(context: Context[Options, Config, Secrets]) -> None:
+    if "GITHUB_ACTIONS" in os.environ:
+        context.config.secrets_path = None  # pragma: nocover
+
+
+entry_point = create_entry_point(
+    main, context, context_creation_callback=configure_no_secret_file
+)
