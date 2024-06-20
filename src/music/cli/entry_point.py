e2d8fb365138032c901cea7845c37629bcbe149f
Quinten <quinten.roets@gmail.com>
2024-06-19 22:42:03 -0700

    remove debug

--- src/music/cli/entry_point.py
+++ src/music/cli/entry_point.py
@@ -8,11 +8,9 @@ from music.models import Config, Options, Secrets
 
 
 def configure_no_secret_file(context: Context[Options, Config, Secrets]) -> None:
-    if "GITHUB_ACTIONS" in os.environ or True:
+    if "GITHUB_ACTIONS" in os.environ:
         context.config.secrets_path = None  # pragma: nocover
 
-
-print("GITHUB_ACTIONS" in os.environ)
 
 entry_point = create_entry_point(
     main, context, context_creation_callback=configure_no_secret_file
