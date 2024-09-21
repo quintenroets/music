13670a2d15e0171d284e434bcd89ae16bc4831d3
Quinten <quinten.roets@gmail.com>
2024-09-20 22:57:27 -0700

    recommend artists in smaller chunks

--- src/music/models/config.py
+++ src/music/models/config.py
@@ -6,7 +6,7 @@ from .path import Path
 @dataclass
 class Config:
     secrets_path = Path.secrets
-    number_of_recommendations: int = 50
+    number_of_recommendations: int = 20
     max_new_recommendation_tries: int = 10
     download_chunk_size: int = 20
     download_retries: int = 5
