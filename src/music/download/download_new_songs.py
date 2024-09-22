3b3cdd53d203b93d22be6853644db3175b0e8665
Quinten <quinten.roets@gmail.com>
2024-09-22 11:45:56 -0700

    test youtube downloading

--- src/music/download/download_new_songs.py
+++ src/music/download/download_new_songs.py
@@ -1,6 +1,7 @@
 import cli
 
 from music.context import context
+from music.models import Path
 from music.utils.batched import batched
 
 from . import spotdl
@@ -21,9 +22,9 @@ def download_youtube_songs() -> None:
         f"https://www.youtube.com/watch?v={id_}"
         for id_ in context.storage.youtube_tracks_to_download
     ]
-    if urls:  # pragma: nocover
+    if urls:
         options = {
-            "output": "%(title)s.%(ext)s",
+            "output": str(Path.processed_songs / "%(title)s.%(ext)s"),
             "embed-thumbnail": None,
             "embed-metadata": None,
             "audio-format": "opus",
