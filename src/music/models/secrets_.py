7e5d1fb757b1987f1c38ab78b76946a554ba6bdf
Quinten <quinten.roets@gmail.com>
2025-01-08 05:09:57 +0000

    pre-commit auto updates

--- src/music/models/secrets_.py
+++ src/music/models/secrets_.py
@@ -1,10 +1,16 @@
-from dataclasses import dataclass
+import os
+from dataclasses import dataclass, field
 
 
 @dataclass
+<<<<<<< HEAD
 class Spotify:
     client_id: str
     client_secret: str
+=======
+class ApiSecrets:
+    token: str = field(default_factory=lambda: os.environ.get("API_TOKEN", ""))
+>>>>>>> template
 
 
 @dataclass
