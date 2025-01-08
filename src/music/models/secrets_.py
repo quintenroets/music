7a5e2cb567781695ec432fc76d50549eab37475d
Quinten Roets <62651391+quintenroets@users.noreply.github.com>
2025-01-07 21:32:46 -0800

    resolve conflicts

--- src/music/models/secrets_.py
+++ src/music/models/secrets_.py
@@ -3,14 +3,9 @@ from dataclasses import dataclass, field
 
 
 @dataclass
-<<<<<<< HEAD
 class Spotify:
     client_id: str
     client_secret: str
-=======
-class ApiSecrets:
-    token: str = field(default_factory=lambda: os.environ.get("API_TOKEN", ""))
->>>>>>> template
 
 
 @dataclass
