e066a69aff4f873d9b15b234304dc9eff5dd7823
Quinten <quinten.roets@gmail.com>
2025-06-19 09:45:34 -0700

    increase test coverage

--- tests/updaters/test_download_ids.py
+++ tests/updaters/test_download_ids.py
@@ -3,3 +3,7 @@ from music import updaters
 
 def test_clean_download_ids() -> None:
     updaters.download_ids.clean_download_ids()
+
+
+def test_check_missing_downloads() -> None:
+    updaters.download_ids.check_missing_downloads()
